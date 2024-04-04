dae654f6c818191de73d35533f7050234b2cfbb4
330namajnoon <sina.cappac@gmail.com>
2024-04-04 17:46:40 -0300

    server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const { Server } = require("sm-express-server");
 const path = require("path");
+const express = require("express");
 
 const port = process.env.PORT || 4005;
 
-const server = new Server(port, "/dist/", []);
+const server = new Server(port, "/", []);
+
+server.app.use("/", express.static(path.join(__dirname, "./dist/")));
+server.app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './dist', 'index.html'));
+});
 
 server.start(() => {
     console.log(`Server is up on port ${port}!`);
