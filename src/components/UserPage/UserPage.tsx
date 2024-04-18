3427f93e363516aa478fb76541116385b0e089c9
330namajnoon <sina.cappac@gmail.com>
2024-04-18 10:11:25 +0200

    MF-003: UserPage steps

--- src/components/UserPage/UserPage.tsx
+++ src/components/UserPage/UserPage.tsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getRooms } from "../../actions/rooms";
 import appContext from "../../contexts/AppContext";
-import { Background } from "./styles";
+import { Background, RoomsListBack, UserNameStepBack } from "./styles";
 import socket from "../../socket";
 import User from "../../modules/User";
 import GameRoom from "../../modules/GameRoom";
@@ -10,66 +10,128 @@ import UserCard from "../UserCard";
 const width = window.innerWidth;
 const height = window.innerHeight;
 
+export type UserPageStatusMap = {
+    ROOMS_LIST: "ROOMS_LIST",
+    USER_NAME: "USER_NAME",
+    AVATARS_LIST: "AVATARS_LIST"
+}
+
 const UserPage = () => {
     const gameState = useContext(appContext);
 
+    const [roomId, setRoomId] = useState<number | null>(null);
+    const [status, setStatus] = useState<keyof UserPageStatusMap>("ROOMS_LIST");
+    const [userState, setUserState] = useState<User>(new User("", "", gameState.avatars.getAvatarFromName("avatar001")));
+
     const roomRegister = (roomId: number, user: User) => {
         socket.emit(`getRoomData`, roomId, user);
+    }
+
+    const setSearchParams = (name: string, value: string) => {
+        const searsh = new URLSearchParams(window.location.search);
+        searsh.set(name, value);
+        history.pushState(null, "",window.location.pathname + "?" + searsh.toString());
+    }
+
+    const getStep = () => {
+        switch (status) {
+            case "ROOMS_LIST":
+                return (
+                    <RoomsListBack>
+                        {gameState.rooms && gameState.rooms.map((room, i) => (
+                            <button onClick={() => {
+                                setRoomId(room.id);
+                                setStatus("USER_NAME");
+                                setSearchParams("roomId", room.id + "");
+                            }} key={i}>
+                                {room.name}
+                            </button>
+                        ))}
+                    </RoomsListBack>
+                )
+            case "USER_NAME":
+                return (
+                    <UserNameStepBack>
+                        UserName
+                    </UserNameStepBack>
+                )
+
+            default:
+                return null;
+        }
     }
 
     useEffect(() => {
         getRooms(gameState);
         socket.emit("login");
         socket.on("login", (id) => {
-            const search = new URLSearchParams(window.location.search);
-            let name = "";
-            if (search.get("userName"))
-                name = search.get("userName") as string;
-            else
-                name = prompt("Tu nombre:") as string;
-            const user = new User(id, name, gameState.avatars.getAvatarFromName("avatar001"));
-            if (search.get("roomId"))
-                roomRegister(parseInt(search.get("roomId") as string), user);
-            gameState.dispatch(gameState.setMe(user));
+            setUserState({ ...userState, id });
         })
+        // socket.on("login", (id) => {
+        //     const search = new URLSearchParams(window.location.search);
+        //     let name = "";
+        //     if (search.get("userName"))
+        //         name = search.get("userName") as string;
+        //     else
+        //         name = prompt("Tu nombre:") as string;
+        //     const user = new User(id, name, gameState.avatars.getAvatarFromName("avatar001"));
+        //     if (search.get("roomId"))
+        //         roomRegister(parseInt(search.get("roomId") as string), user);
+        //     gameState.dispatch(gameState.setMe(user));
+        // })
 
-        socket.on(`${gameState.currentRoom?.roomData.id}_sendRoomData`, (room: GameRoom) => {
-            console.log(room);
-        });
+        // socket.on(`${gameState.currentRoom?.roomData.id}_sendRoomData`, (room: GameRoom) => {
+        //     console.log(room);
+        // });
 
-        socket.on("sendCurrentRoom", (room: GameRoom) => {
-            if (gameState.currentRoom) {
-                gameState.updateCurrentRoom(room);
-            } else {
-                console.log(room)
-                gameState.setCurrentRoom(room.roomData, null);
-                gameState.dispatch(gameState.updateCurrentRoom(room));
-            }
-            gameState.start();
-        })
-        return () => {
-            socket.off("login", (id) => {
-                console.log(id);
-            });
-        }
+        // socket.on("sendCurrentRoom", (room: GameRoom) => {
+        //     if (gameState.currentRoom) {
+        //         gameState.updateCurrentRoom(room);
+        //     } else {
+        //         console.log(room)
+        //         gameState.setCurrentRoom(room.roomData, null);
+        //         gameState.dispatch(gameState.updateCurrentRoom(room));
+        //     }
+        //     gameState.start();
+        // })
+        // return () => {
+        //     socket.off("login", (id) => {
+        //         console.log(id);
+        //     });
+        // }
     }, [])
 
     useEffect(() => {
-        if (gameState.rooms)
+        if (gameState.rooms) {
+            switch (status) {
+                case "ROOMS_LIST":
+                    const id = new URLSearchParams(window.location.search).get("roomId");
+                    if (id) {
+                        setRoomId(parseInt(id));
+                        setStatus("USER_NAME");
+                    }
+                    break;
+                case "USER_NAME":
+                    const userName = new URLSearchParams(window.location.search).get("userName");
+                    if (userName) {
+                        setUserState({ ...userState, name: userName });
+                        setStatus("AVATARS_LIST");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
             console.log(gameState.rooms)
-    }, [gameState.rooms]);
+        }
+    }, [gameState.rooms, status]);
 
-    if (gameState.me && gameState.currentRoom) {
-        const me = gameState.getUserById(gameState.me.id);
-        if (me)
-            return (
-                <Background>
-                    <UserCard width={width} height={height} user={me} />
-                </Background>
-            )
-    }
-
-    return null;
+    
+    return (
+        <Background>
+            {getStep()}
+        </Background>
+    )
 
 }
 
--- src/components/UserPage/styles.ts
+++ src/components/UserPage/styles.ts
@@ -1,9 +1,46 @@
 import styled from "styled-components";
+import { colors } from "../../config";
 
 export const Background = styled.div`
-
+    width: 100vw;
+    height: 100vh;
+    background-image: url(./images/start_back.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 `;
 
 export const RoomsBack = styled.div`
     width: 100%;
+`;
+
+export const RoomsListBack = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 30px;
+    box-sizing: border-box;
+    button {
+        width: 300px;
+        padding: 10px;
+        font-size: 20px;
+        background-color: ${colors[5]};
+        border-radius: 5px;
+        border: solid 0.5px ${colors[2]};
+        color: ${colors[0]};
+        margin-top: 10px;
+        
+    }
+`;
+
+export const UserNameStepBack = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `;
